5c3b595716028d63c968200325f2e6b4bd115d14
fran_cap15 <balter.contacto@gmail.com>
2025-01-17 21:40:22 -0300

    portfolio updated

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("nav ul li a");
+const btn = document.getElementById('button');
+const sectionAll = document.querySelectorAll('section[id]');
+const inputName = document.querySelector('#nombre');
+const inputEmail = document.querySelector('#email');
+const flagsElement = document.getElementById('flags');
+const textsToChange = document.querySelectorAll('[data-section]');
 
-    links.forEach(link => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute("href"));
-            window.scrollTo({
-                top: target.offsetTop - 60,
-                behavior: "smooth"
-            });
-        });
+/* ===== Loader =====*/
+window.addEventListener('load', () => {
+    const contenedorLoader = document.querySelector('.container--loader');
+    contenedorLoader.style.opacity = 0;
+    contenedorLoader.style.visibility = 'hidden';
+})
+
+/*===== Header =====*/
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('abajo', window.scrollY > 0);
+});
+
+/*===== Boton Menu =====*/
+btn.addEventListener('click', function() {
+    if (this.classList.contains('active')) {
+        this.classList.remove('active');
+        this.classList.add('not-active');
+        document.querySelector('.nav_menu').classList.remove('active');
+        document.querySelector('.nav_menu').classList.add('not-active');
+    }
+    else {
+        this.classList.add('active');
+        this.classList.remove('not-active');
+        document.querySelector('.nav_menu').classList.remove('not-active');
+        document.querySelector('.nav_menu').classList.add('active');
+    }
+});
+
+/*===== class active por secciones =====*/
+window.addEventListener('scroll', () => {
+    const scrollY = window.pageYOffset;
+    sectionAll.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 100;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+            document.querySelector('nav a[href*=' + sectionId + ']').classList.add('active');
+        }
+        else {
+            document.querySelector('nav a[href*=' + sectionId + ']').classList.remove('active');
+        }
     });
 });
 
-document.getElementById("backToTop").addEventListener("click", function() {
+/*===== Boton y función ir arriba =====*/
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container').classList.add('show');
+    }
+    else {
+        document.querySelector('.go-top-container').classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
     window.scrollTo({
         top: 0,
-        behavior: "smooth"
+        behavior: 'smooth'
     });
-});
-
-const links = [
-    {
-        url: 'https://www.linkedin.com/in/balter-velázquez',
-        text: 'Balter Velázquez',
-        iconClass: 'fab fa-linkedin'
-    },
-    {
-        url: 'https://github.com/francap15',
-        text: 'Balter Velázquez',
-        iconClass: 'fab fa-github'
-    }
-];
-
-const linksContainer = document.getElementById('linksContainer');
-
-links.forEach(link => {
-    const linkElement = document.createElement('a');
-    linkElement.href = link.url;
-    linkElement.target = '_blank';
-
-    const iconElement = document.createElement('i');
-    iconElement.className = link.iconClass;
-
-    const textElement = document.createTextNode(link.text);
-
-    linkElement.appendChild(iconElement);
-    linkElement.appendChild(textElement);
-
-    linksContainer.appendChild(linkElement);
 });
